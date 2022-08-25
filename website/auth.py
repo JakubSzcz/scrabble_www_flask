70796b37866d4072f543258d3512af6d9a6f1f9b
miloszjaneczek <mjaneczek@student.agh.edu.pl>
2022-08-26 00:19:13 +0200

    potencjalnie logowanie i rejestracja ale nwm wsm

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,47 @@
-from flask import Blueprint
-
+from flask import Blueprint, render_template, request, redirect
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/')
 def login():
-    return "<p>Login</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user, remember=True)
+                return redirect('/main')
+            else:
+                #niepoprawne haslo
+                return
+        else:
+            #nie ma takiego maila
+            return
 
-@auth.route('/logout')
-def logout():
-    return "<p>Logout</p>"
+    return render_template("login.html", user=current_user)
 
-@auth.route('/signup')
-def signup():
-    return "<p>Sign up</p>"
+
+@auth.route('/new_account', methods = ['GET', 'POST'])
+def new_account():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            # email zajety
+            return
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            return redirect('/main')
+
+    return render_template("new_account.html")
