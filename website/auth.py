bf01064d57e766a52483cabe685e25ba6eb21803
Jakub <jszczudlik@student.agh.edu.pl>
2022-09-01 23:04:53 +0200

    nowy model Game, nowa baza, wyswietla historie gier w main

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,7 @@
 from email import message
+from lib2to3.pgen2.literals import test
 from flask import Blueprint, render_template, request, redirect, url_for
-from .models import User
+from .models import User, Game
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
@@ -42,6 +43,21 @@ def new_account():
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password, method='sha256'))
             db.session.add(new_user)
+            db.session.commit()
+            test_game0 = Game(winner="test0", time="test0", score="test0", user=new_user)
+            test_game1 = Game(winner="test1", time="test1", score="test1", user=new_user)
+            test_game2 = Game(winner="test2", time="test2", score="test2", user=new_user)
+            test_game3 = Game(winner="test3", time="test3", score="test3", user=new_user)
+            test_game4 = Game(winner="test4", time="test4", score="test4", user=new_user)
+            db.session.add(test_game0)
+            db.session.commit()
+            db.session.add(test_game1)
+            db.session.commit()
+            db.session.add(test_game2)
+            db.session.commit()
+            db.session.add(test_game3)
+            db.session.commit()
+            db.session.add(test_game4)
             db.session.commit()
             login_user(new_user, remember=True)
             return redirect('/main')
--- website/models.py
+++ website/models.py
@@ -4,6 +4,18 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique = True)
+    email = db.Column(db.String(150), unique=True, nullable="False")
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    games = db.relationship('Game', backref='user') 
+    #user.games[0] -> lista gier
+    
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    winner = db.Column(db.String(150))
+    time = db.Column(db.String(150))
+    score = db.Column(db.String(20))
+    plansza = db.Column(db.String())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    #game.user.first_name-nazwa
