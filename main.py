cba06efab16ec76c775a9d0ef93f9c107c3b00ef
miloszjaneczek <mjaneczek@student.agh.edu.pl>
2022-09-06 22:15:59 +0200

    Dodano pokoje

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 from unicodedata import name
 from website import create_app
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
 
 app = create_app()
 socketio = SocketIO(app, cors_allowed_origins='*')
 users = [] #lista z graczami
+listaGier = {}
 
 
+@socketio.on("dolacz_do_gry")
+def dolaczDoGry(numer, haslo):
+    if numer not in listaGier:
+        listaGier[numer] = haslo #tworze pokoj
+        join_room(numer)
+    else: # dołączam do już istniejącego pokoju
+        if listaGier[numer] == haslo:
+            join_room(numer)
+        else:
+            #złe hasło
+            return
 
 @socketio.on("odbierz_plansze")
-def odbierzPlansze(plansza, czyjaTura):
+def odbierzPlansze(plansza, czyjaTura, numer):
     data = {"plansza":plansza, "czyjaTura" : czyjaTura}
-    emit("message", data, broadcast=True)
+    send("message", data, to=numer)
 
 @socketio.on("lista_graczy")
-def lista_graczy(userName):
+def lista_graczy(userName, numer):
     if len(users) == 0: #pierwszy gracz
         users.append(userName)
     else:
@@ -26,7 +38,7 @@ def lista_graczy(userName):
         if flaga:
             users.append(userName)
 
-    emit("odbierz_liste_graczy", users, broadcast=True)
+    send("odbierz_liste_graczy", users, to=numer)
 
 
 
